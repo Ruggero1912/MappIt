ed3376d8c22029beffe942ece713c7152c746df9
Giacomo Pacini <giacomopacini98@gmail.com>
2021-12-20 17:06:30 +0100

    Post & YTPost constructor

--- dataPopulation/YTposts/YTPost.py
+++ dataPopulation/YTposts/YTPost.py
@@ -14,7 +14,10 @@ class YTPost(Post):
 
     BEGIN               = start()
 
-    KEY_YT_CHANNEL      = os.getenv("USER_YT_CHANNEL_ID_KEY")
-    KEY_ID              = os.getenv("USER_ID_KEY")
+    KEY_YT_CHANNEL      = os.getenv("YTPOST_USER_YT_CHANNEL_ID_KEY")
+    KEY_YT_VIDEO_ID     = os.getenv("YTPOST_YT_VIDEO_ID")
 
-    #TODO: the constructor, that should also call the super method
+    def __init__(self, author_id, yt_video_id : str, yt_channel_id, title=None, description=None, post_date: datetime = None, exp_date=None, tags_array: list = ..., activity=None, pics_array: list = ..., thumbnail=None) -> None:
+        setattr(self, YTPost.KEY_YT_VIDEO_ID, yt_video_id  )
+        setattr(self, YTPost.KEY_YT_CHANNEL , yt_channel_id)
+        super().__init__(author_id, title=title, description=description, post_date=post_date, exp_date=exp_date, tags_array=tags_array, activity=activity, pics_array=pics_array, thumbnail=thumbnail)
--- dataPopulation/posts/Post.py
+++ dataPopulation/posts/Post.py
@@ -14,7 +14,8 @@ class Post:
 
     BEGIN               = start()
 
-
+    DEFAULT_ACTIVITY    = os.getenv("POST_GENERIC_DEFAULT_ACTIVITY")
+    DEFAULT_THUMBNAIL   = os.getenv("POST_DEFAULT_THUMBNAIL")
 
     fake                = Utils.fake
 
@@ -29,9 +30,36 @@ class Post:
     KEY_ACTIVITY        = os.getenv("POST_ACTIVITY_KEY")
     KEY_TAGS            = os.getenv("POST_TAGS_KEY")
 
-    def __init__(self, title=None, description=None, post_date=None, exp_date=None) -> None:
+    def __init__(self, author_id, title=None, description=None, post_date : datetime =None, exp_date=None, tags_array : list =[], activity=None, pics_array : list =[], thumbnail=None) -> None:
         setattr(self, Post.KEY_TITLE            , title         if title        is not None else Post.fake.sentence(nb_words=4)        ) #short sentence as fake title
         setattr(self, Post.KEY_DESC             , description   if description  is not None else Post.fake.paragraph()                 )
+
+        setattr(self, Post.KEY_PICTURES         , pics_array    if len(pics_array)          else []                                    )
+        pics = self.get_pics_array()
+        setattr(self, Post.KEY_THUMBNAIL        , thumbnail     if thumbnail    is not None else pics[0] if len(pics) else Post.DEFAULT_THUMBNAIL)                                    )
+        setattr(self, Post.KEY_TAGS             , tags_array    if len(tags_array)          else []                                    )
+        setattr(self, Post.KEY_ACTIVITY         , activity      if activity     is not None else self.determine_activity()             )
         setattr(self, Post.KEY_POST_DATE        , post_date     if post_date    is not None else datetime.now()                        )
-        setattr(self, Post.KEY_EXPERIENCE_DATE  , exp_date      if exp_date     is not None else Post.fake.date()                        )
-        #TODO: complete the constructor with the other fields
+        post_date = self.get_post_datetime().date()
+        setattr(self, Post.KEY_EXPERIENCE_DATE  , exp_date      if exp_date     is not None else Post.fake.date_between(end_date=post_date, start_date='-3y')                      )
+
+        #MANDATORY PARAMETERS:
+        setattr(self, Post.KEY_AUTHOR           , author_id     )
+
+        
+    def determine_activity(self):
+        """
+        returns the activity that is supposed to happen in this post, based on title, description and tags
+        """
+        #TODO: method to parse the activity from text (a call to a method or here inline)
+        return Post.DEFAULT_ACTIVITY
+
+    def get_pics_array(self):
+        pics = getattr(self, Post.KEY_PICTURES)
+        assert isinstance(pics, list)
+        return pics
+
+    def get_post_datetime(self) -> datetime:
+        post_datetime = getattr(self, Post.KEY_POST_DATE)
+        assert isinstance(post_datetime, datetime)
+        return post_datetime
