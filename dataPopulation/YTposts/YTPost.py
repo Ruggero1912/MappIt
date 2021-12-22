6794af5042b7834b04679de1146810e7eaabf894
Giacomo Pacini <giacomopacini98@gmail.com>
2021-12-22 12:00:46 +0100

    doesn't put in the dict (to be stored in the db) the YT channel id

--- dataPopulation/YTposts/YTPost.py
+++ dataPopulation/YTposts/YTPost.py
@@ -17,7 +17,9 @@ class YTPost(Post):
     KEY_YT_CHANNEL      = os.getenv("YTPOST_USER_YT_CHANNEL_ID_KEY")
     KEY_YT_VIDEO_ID     = os.getenv("YTPOST_YT_VIDEO_ID")
 
-    def __init__(self, author_id, yt_video_id : str, yt_channel_id, title=None, description=None, post_date: datetime = None, exp_date=None, tags_array: list = ..., activity=None, pics_array: list = ..., thumbnail=None) -> None:
+    DICT_IGNORED_ATTRIBUTES = Post.DICT_IGNORED_ATTRIBUTES + [KEY_YT_CHANNEL]  #we won't store in mongo the channelID, it is redundant
+
+    def __init__(self, author_id, yt_video_id : str, yt_channel_id, title=None, description=None, post_date: datetime = None, exp_date=None, tags_array: list = [], activity=None, pics_array: list = [], thumbnail=None) -> None:
         setattr(self, YTPost.KEY_YT_VIDEO_ID, yt_video_id  )
         setattr(self, YTPost.KEY_YT_CHANNEL , yt_channel_id)
         super().__init__(author_id, title=title, description=description, post_date=post_date, exp_date=exp_date, tags_array=tags_array, activity=activity, pics_array=pics_array, thumbnail=thumbnail)
