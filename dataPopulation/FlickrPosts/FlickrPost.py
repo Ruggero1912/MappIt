838e113d60793f43c2a18078680641bb6c5ef9ba
Ruggero1912 <63967908+Ruggero1912@users.noreply.github.com>
2022-01-02 13:37:14 +0100

    flickrPost bean class

---
+++ dataPopulation/FlickrPosts/FlickrPost.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from utilities.utils import Utils
+from posts.Post import Post
+
+class FlickrPost(Post):
+
+    #TODO: write here possible additional attribute to be added for flickr posts
+    #(i. e.):
+    # - flickr post link / flickr post id
+    # - ??
+
+    KEY_FLICKR_POST_ID      = Utils.load_config("POST_FLICKR_POST_ID")
+
+    DICT_IGNORED_ATTRIBUTES = Post.DICT_IGNORED_ATTRIBUTES + []
+
+    def __init__(self, author_id, flickr_post_id : str, title=None, description=None, post_date: datetime = None, exp_date=None, tags_array: list = [], activity=None, pics_array: list = [], thumbnail=None) -> None:
+
+        setattr(self, FlickrPost.KEY_FLICKR_POST_ID, flickr_post_id)
+
+        super().__init__(author_id, title=title, description=description, post_date=post_date, exp_date=exp_date, tags_array=tags_array, activity=activity, pics_array=pics_array, thumbnail=thumbnail)
