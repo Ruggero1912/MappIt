10019417b2c80f9595f268622f19daa9301990d0
Ruggero1912 <63967908+Ruggero1912@users.noreply.github.com>
2022-01-06 13:08:43 +0100

    PlaceManager methods documentation and PlaceManagerMongoDB getPlacesInRadius and getPlaceFromId methods implementation

--- Server/src/main/java/it/unipi/dii/inginf/lsmsdb/mapsproject/place/persistence/information/PlaceManager.java
+++ Server/src/main/java/it/unipi/dii/inginf/lsmsdb/mapsproject/place/persistence/information/PlaceManager.java
@@ -7,9 +7,33 @@ import java.util.List;
 
 public interface PlaceManager {
 
+    /**
+     * return null if it does not exist a place with that id, otherwise returns the associated place
+     * @param id the id of the requested place
+     * @return associated Place object or null if not found
+     */
     Place getPlaceFromId(String id);
 
-    List<Place> getPlacesGivenCoordinate(Coordinate coordinate);
+    /**
+     * returns a list of places that are in the radius, ordered by a certain criteria
+     * @param coordinates a Coordinates object representing the center of the zone in which it searches
+     * @param radius (in km) the radius of the zone in which to search
+     * @param orderBy the criteria for which the results should be ordinated
+     * @return a list of Place object, else null if empty set
+     */
+    List<Place> getPlacesInRadius(Coordinate coordinates, Double radius, String orderBy);
+
+    /**
+     * returns a list of places that are in the radius, ordered by a certain criteria
+     * @param coordinates a Coordinates object representing the center of the zone in which it searches
+     * @param radius (in km) the radius of the zone in which to search
+     * @param orderBy the criteria for which the results should be ordinated
+     * @param activityFilter the activity that the returned Places should fit
+     * @return a list of Place object, else null if empty set
+     */
+    List<Place> getPlacesInRadiusFilteredByFits(Coordinate coordinates, Double radius, String orderBy, String activityFilter);
+
+
 
     List<Place> getPlacesGivenCoordinateAndActivity(Coordinate coordinate, String activity);
 
--- Server/src/main/java/it/unipi/dii/inginf/lsmsdb/mapsproject/place/persistence/information/PlaceManagerMongoDB.java
+++ Server/src/main/java/it/unipi/dii/inginf/lsmsdb/mapsproject/place/persistence/information/PlaceManagerMongoDB.java
@@ -1,8 +1,10 @@
 package it.unipi.dii.inginf.lsmsdb.mapsproject.place.persistence.information;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.geojson.Point;
 import it.unipi.dii.inginf.lsmsdb.mapsproject.persistence.connection.MongoConnection;
 import it.unipi.dii.inginf.lsmsdb.mapsproject.place.Coordinate;
 import it.unipi.dii.inginf.lsmsdb.mapsproject.place.Place;
@@ -11,6 +13,7 @@ import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -31,14 +34,55 @@ public class PlaceManagerMongoDB implements PlaceManager{
 
     @Override
     public Place getPlaceFromId(String id) {
-        //...
-        return null;
+        if(id.equals(""))
+            return  null;
+
+        ObjectId objId;
+        try{
+            objId = new ObjectId(id);
+        } catch (Exception e){
+            LOGGER.log(Level.SEVERE, e.getMessage());
+            return null;
+        }
+        Bson idFilter = Filters.eq(IDKEY, objId);
+        MongoCursor<Document> cursor = placeCollection.find(idFilter).cursor();
+        if(!cursor.hasNext()){
+            return null;
+        }else{
+            Document placeDoc = cursor.next();
+            Place ret = new Place(placeDoc);
+            return ret;
+        }
+    }
+
+    /**
+     * use this method to query the Place Collection for Places instances
+     * @param filters : a Bson object representing the filters to use to query the place Collection
+     * @return null if empty set, else a List of Places respecting the given filters
+     */
+    private List<Place> queryPlaceCollection(Bson filters){
+        List<Place> places = new ArrayList<>();
+        FindIterable<Document> iterable = placeCollection.find(filters);
+        iterable.forEach(doc -> places.add(new Place(doc)));
+        return places;
     }
 
     @Override
-    public List<Place> getPlacesGivenCoordinate(Coordinate coordinate) {
-        //...
-        return null;
+    public List<Place> getPlacesInRadius(Coordinate coordinates, Double radius, String orderBy) {
+        Double radiusInMeters = radius * 1000.0;
+        Point point = coordinates.toPoint();
+        Bson radiusFilter = Filters.near(Place.KEY_LOC, point, radiusInMeters, null);
+        return this.queryPlaceCollection(radiusFilter);
+    }
+
+    @Override
+    public List<Place> getPlacesInRadiusFilteredByFits(Coordinate coordinates, Double radius, String orderBy, String activityName) {
+        Double radiusInMeters = radius * 1000.0;
+        Point point = coordinates.toPoint();
+        Bson radiusFilter = Filters.near(Place.KEY_LOC, point, radiusInMeters, null);
+        Bson activityFilter = Filters.in(Place.KEY_FITS, activityName);
+        Bson filter = Filters.and(radiusFilter, activityFilter);
+        return this.queryPlaceCollection(filter);
     }
 
     @Override
