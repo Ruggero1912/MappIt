739ed2fb7fc3b6309eafd6957d99e95be5708e33
enricollen <nello.enrico@gmail.com>
2022-01-17 10:41:16 +0100

    Fixed Exception Handlingn & responses for PlaceController

--- Server/src/main/java/it/unipi/dii/inginf/lsmsdb/mapsproject/controller/PlaceController.java
+++ Server/src/main/java/it/unipi/dii/inginf/lsmsdb/mapsproject/controller/PlaceController.java
@@ -7,6 +7,8 @@ import it.unipi.dii.inginf.lsmsdb.mapsproject.httpAccessControl.UserSpring;
 import it.unipi.dii.inginf.lsmsdb.mapsproject.place.Coordinate;
 import it.unipi.dii.inginf.lsmsdb.mapsproject.place.Place;
 import it.unipi.dii.inginf.lsmsdb.mapsproject.place.PlaceService;
+import it.unipi.dii.inginf.lsmsdb.mapsproject.post.Post;
+import it.unipi.dii.inginf.lsmsdb.mapsproject.post.PostService;
 import it.unipi.dii.inginf.lsmsdb.mapsproject.user.User;
 import it.unipi.dii.inginf.lsmsdb.mapsproject.user.UserService;
 import org.springframework.http.HttpStatus;
@@ -18,97 +20,157 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RequestMapping("/api")
 @RestController
 @SecurityRequirement(name = "bearerAuth")
 public class PlaceController {
 
+    private static final Logger LOGGER = Logger.getLogger( PlaceService.class.getName() );
+
     @ApiOperation(value = "Get information of a specific place", notes = "This method retrieve information of a specific place, given its _id")
     @GetMapping(value = "/place/{id}", produces = "application/json")
-    public Place getPlaceById(@PathVariable(value = "id") String placeId) {
-        return PlaceService.getPlaceFromId(placeId);
+    public ResponseEntity<?> getPlaceById(@PathVariable(value = "id") String placeId) {
+        ResponseEntity<?> result;
+
+        try{
+            Place place = PlaceService.getPlaceFromId(placeId);
+            result = ResponseEntity.ok(place);
+            if(place==null) {
+                LOGGER.log(Level.WARNING, "Error: could not find place (id=" + placeId + ")");
+                result = ResponseEntity.status(HttpStatus.FORBIDDEN).body("{\"Error\" : \"Could not find place\"}");
+            }
+        } catch (Exception e){
+            LOGGER.log(Level.WARNING, "Error: could not parse place, an exception has occurred: " + e);
+            result = ResponseEntity.status(HttpStatus.FORBIDDEN).body("{\"Error\" : \"Could not parse place, and exception has occurred\"}");
+        }
+        return result;
     }
 
     // popular places in a given radius (nearby or near to a given point)
     @ApiOperation(value = "returns a list of places near the given coordinates in a certain radius (in km), if an activity is specified, returns only the ones that fits to that activity")
     @GetMapping(value = "/places/nearby", produces = "application/json")
-    public List<Place> nearbyPlaces(@RequestParam Double lat, @RequestParam Double lon,
+    public ResponseEntity<?> nearbyPlaces(@RequestParam Double lat, @RequestParam Double lon,
                                     @RequestParam( defaultValue = "10.0") Double radius,
                                     @RequestParam( defaultValue = "any") String activityFilter,
                                     @RequestParam( defaultValue = "distance") String orderBy) {
-        // check the validity of the given activity
-        // check the validity of the given radius (in km)
-        // parse the coordinates from lon, lat
-        Coordinate coordinates = new Coordinate(lat, lon);
+        ResponseEntity<?> result;
+        List<Place> nearbyPlaces;
 
-        // the orderBy String content is checked by PlaceService
-        if(activityFilter == "any"){
-          return PlaceService.getPlacesInRadius(coordinates, radius, orderBy);
+        try {
+            //TODO: check the validity of the given activity
+            // check the validity of the given radius (in km)
+            // parse the coordinates from lon, lat
+            Coordinate coordinates = new Coordinate(lat, lon);
+
+            // the orderBy String content is checked by PlaceService
+            if (activityFilter == "any") {
+                nearbyPlaces = PlaceService.getPlacesInRadius(coordinates, radius, orderBy);
+            } else {
+                nearbyPlaces = PlaceService.getPlacesInRadius(coordinates, radius, orderBy, activityFilter);
+            }
+
+            result = ResponseEntity.status(HttpStatus.OK).body(nearbyPlaces);
+        }catch (Exception e) {
+            result = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"Error\":\"something went wrong in getting nearby places\"}");
         }
-        else{
-          return PlaceService.getPlacesInRadius(coordinates, radius, orderBy, activityFilter);
-        }
+
+        return result;
     }
     // most popular places
     // most popular places for given category
     @ApiOperation(value = "returns a list of popular places")
     @GetMapping(value = "/places/most-popular", produces = "application/json")
-    public List<Place> popularPlaces(@RequestParam( defaultValue = "any", name = "activity") String activityFilter, @RequestParam(defaultValue = "0", name = "limit") int maxQuantity) {
-    return PlaceService.getPopularPlaces(activityFilter, maxQuantity);
+    public ResponseEntity<?> popularPlaces(@RequestParam( defaultValue = "any", name = "activity") String activityFilter, @RequestParam(defaultValue = "0", name = "limit") int maxQuantity) {
+        ResponseEntity<?> result;
+
+        try {
+            List<Place> popularPlaces = PlaceService.getPopularPlaces(activityFilter, maxQuantity);
+            result = ResponseEntity.status(HttpStatus.OK).body(popularPlaces);
+        }catch (Exception e) {
+            result = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"Error\":\"something went wrong in getting popular places\"}");
+        }
+
+        return result;
     }
 
     // suggested places for the current user
     @ApiOperation(value = "returns a list of suggested places for the current user")
     @GetMapping(value = "/places/suggested", produces = "application/json")
-    public List<Place> suggestedPlaces() {
-        UserSpring userSpring = (UserSpring) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        User currentUser = userSpring.getApplicationUser();
-        return PlaceService.getSuggestedPlaces(currentUser);
+    public ResponseEntity<?> suggestedPlaces() {
+        ResponseEntity<?> result;
+        try {
+            UserSpring userSpring = (UserSpring) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            User currentUser = userSpring.getApplicationUser();
+            List<Place> suggestedPlaces = PlaceService.getSuggestedPlaces(currentUser);
+            result = ResponseEntity.status(HttpStatus.OK).body(suggestedPlaces);
+        }catch (Exception e) {
+            result = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"Error\":\"something went wrong in getting suggested places\"}");
+        }
+
+        return result;
     }
     // your favourite places
     // places that are favourites of a given user (it should receive the id of the user)
     @ApiOperation(value = "returns the list of favourite places for the specified user or for the current if no userId is specified")
     @GetMapping(value = "/places/favourites", produces = "application/json")
     public ResponseEntity<?> favouritePlaces(@RequestParam( defaultValue = "current") String userId) {
+        ResponseEntity<?> result;
         User u;
-        if(userId == "current"){
-            //retrieve the current user
-            UserSpring userSpring = (UserSpring) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            u = userSpring.getApplicationUser();
-        }else{
-            u = UserService.getUserFromId(userId);
+
+        try {
+            if (userId == "current") {
+                //retrieve the current user
+                UserSpring userSpring = (UserSpring) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                u = userSpring.getApplicationUser();
+            } else {
+                u = UserService.getUserFromId(userId);
+            }
+            List<Place> places = UserService.getFavouritePlaces(u);
+            result = ResponseEntity.status(HttpStatus.OK).body(places);
+        }catch (Exception e) {
+            result = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"Error\":\"something went wrong in getting favourite places\"}");
         }
-        List<Place> places = UserService.getFavouritePlaces(u);
-        return ResponseEntity.status(HttpStatus.OK).body(places);
+
+        return result;
     }
     // places that you visited
     // places visited by a given user
     @ApiOperation(value = "returns the list of visited places for the specified user or for the current if no userId is specified")
     @GetMapping(value = "/places/visited", produces = "application/json")
     public ResponseEntity<?> visitedPlaces(@RequestParam( defaultValue = "current") String userId) {
+        ResponseEntity<?> result;
         User u;
-        if(userId == "current"){
-            //retrieve the current user
-            UserSpring userSpring = (UserSpring) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            u = userSpring.getApplicationUser();
 
-        }else{
-            u = UserService.getUserFromId(userId);
+        try {
+            if (userId == "current") {
+                //retrieve the current user
+                UserSpring userSpring = (UserSpring) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                u = userSpring.getApplicationUser();
+            } else {
+                u = UserService.getUserFromId(userId);
+            }
+            List<Place> places = UserService.getVisitedPlaces(u);
+            result = ResponseEntity.status(HttpStatus.OK).body(places);
+        } catch (Exception e) {
+            result = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"Error\":\"something went wrong in getting visited places\"}");
         }
-        List<Place> places = UserService.getVisitedPlaces(u);
-        return ResponseEntity.status(HttpStatus.OK).body(places);
+
+        return result;
     }
 
     // add to favourite a place (wants the id of the place)
     // remove from favourite a place (wants the id of the place)
     @ApiOperation(value = "adds/removes the specified place to the favourite places of the currently logged in user")
     @PostMapping(value = "/place/{id}/favourites/{action}", produces = "application/json")
-    public ResponseEntity handleFavourites(@PathVariable(name="id") String placeId, @PathVariable(name = "action") String action) {
+    public ResponseEntity<?> handleFavourites(@PathVariable(name="id") String placeId, @PathVariable(name = "action") String action) {
+        ResponseEntity<?> result;
+
         //should retrieve the place object (and check if it exists)
         Place place = PlaceService.getPlaceFromId(placeId);
         if(place == null){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"the specified place does not exist\"}");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"Error\":\"the specified place does not exist\"}");
         }
         //retrieve the current user
         UserSpring userSpring = (UserSpring) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -118,39 +180,48 @@ public class PlaceController {
         actions.add("add");
         actions.add("remove");
         if( ! actions.contains(action)){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"action not allowed\"}");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"Error\":\"action not allowed\"}");
         }
-        // TODO: handle the outcome of the method call and answer to the client consequently
+
         try {
             if (action == "add")
                 UserService.addPlaceToFavourites(u, place);
             else if (action == "remove")
                 UserService.removePlaceFromFavourites(u, place);
+            result = ResponseEntity.status(HttpStatus.OK).body("{\"Success\":\" correctly updated the favourites\"}");
         }catch (DatabaseErrorException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\":\"something went wrong\"}");
+            result = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"Error\":\"something went wrong in querying Databases\"}");
+        }catch (Exception e) {
+            result = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"Error\":\"something went wrong in updating favourites\"}");
         }
 
-        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body("{\"error\":\"handle correctly the response\"}");
+        return result;
     }
     // add to visited a place (wants the id of the place)
     @ApiOperation(value = "adds the specified place to the visited places of the currently logged in user")
     @PostMapping(value = "/place/{id}/visited", produces = "application/json")
-    public ResponseEntity handleVisited(@PathVariable(name="id") String placeId, @RequestBody(required = false) LocalDateTime localDateTime) {
+    public ResponseEntity<?> handleVisited(@PathVariable(name="id") String placeId, @RequestBody(required = false) LocalDateTime localDateTime) {
+        ResponseEntity<?> result;
+
         if(localDateTime == null){
             // the time of the visit will be considered now
             localDateTime = LocalDateTime.now();
         }
-        //should retrieve the place object (and check if it exists)
-        Place place = PlaceService.getPlaceFromId(placeId);
-        if(place == null){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"the specified place does not exist\"}");
+        try {
+            //should retrieve the place object (and check if it exists)
+            Place place = PlaceService.getPlaceFromId(placeId);
+            if (place == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"Error\":\"the specified place does not exist\"}");
+            }
+            //retrieve the current user
+            UserSpring userSpring = (UserSpring) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            User u = userSpring.getApplicationUser();
+            UserService.addPlaceToVisited(u, place, localDateTime);
+            result = ResponseEntity.status(HttpStatus.OK).body("{\"Success\":\" correctly added the visited place\"}");
+        }catch (Exception e) {
+            result = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"Error\":\"something went wrong in inserting visited place\"}");
         }
-        //retrieve the current user
-        UserSpring userSpring = (UserSpring) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        User u = userSpring.getApplicationUser();
 
-        // TODO: handle the outcome of the method call and answer to the client consequently
-        UserService.addPlaceToVisited(u, place, localDateTime);
-        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body("{\"error\":\"handle correctly the response\"}");
+        return result;
     }
 }
