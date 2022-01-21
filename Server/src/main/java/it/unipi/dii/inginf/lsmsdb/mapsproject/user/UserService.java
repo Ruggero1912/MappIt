24f03c102c1144764e5202faa5ce1ba5cd6070f6
Ruggero1912 <63967908+Ruggero1912@users.noreply.github.com>
2022-01-21 12:02:19 +0100

    PostService.deletePostsOfGivenUser method implementation

--- Server/src/main/java/it/unipi/dii/inginf/lsmsdb/mapsproject/user/UserService.java
+++ Server/src/main/java/it/unipi/dii/inginf/lsmsdb/mapsproject/user/UserService.java
@@ -182,6 +182,7 @@ public class UserService {
 
         String userId = userToDelete.getId();
 
+        //TODO: call to PostService.deletePostsOfGivenUser instead of using the PostManager's methods
         PostManager pm = PostManagerFactory.getPostManager();
         boolean PostsDeletedFromMongo = pm.deletePostsOfGivenUser(userToDelete);
 
