898cd854b743dfeb24fe6e4b1d4cc63d5ab24923
Ruggero1912 <63967908+Ruggero1912@users.noreply.github.com>
2022-01-26 18:55:30 +0100

    added optional attribute timeVisit

--- Server/src/main/java/it/unipi/dii/inginf/lsmsdb/mapsproject/place/PlacePreview.java
+++ Server/src/main/java/it/unipi/dii/inginf/lsmsdb/mapsproject/place/PlacePreview.java
@@ -1,11 +1,16 @@
 package it.unipi.dii.inginf.lsmsdb.mapsproject.place;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.neo4j.driver.Value;
 
 public class PlacePreview {
 
+    public final static String NEO_KEY_TIME_VISIT = "datetime";
+
     private String _id;
     private String name;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String timeVisit;
 
     /**
      * We exploit this constructor to parse a Place Preview object from a Neo4j Node
@@ -16,6 +21,17 @@ public class PlacePreview {
         this.name =  valueFromPlaceNode.get(Place.KEY_NAME).asString();
     }
 
+    /**
+     * We exploit this constructor to parse a Place Preview object from a Neo4j Node
+     * @param valueFromPlaceNode corresponds to the value gathered from the Place Node in Neo4j
+     * @param visitInfos the Neo4j infos about a visit done by a given user to that place
+     */
+    public PlacePreview(Value valueFromPlaceNode, Value visitInfos) {
+        this._id =  valueFromPlaceNode.get(Place.NEO_KEY_ID).asString();
+        this.name =  valueFromPlaceNode.get(Place.KEY_NAME).asString();
+        this.timeVisit = visitInfos.get(PlacePreview.NEO_KEY_TIME_VISIT).toString();
+    }
+
     public String get_id() {
         return _id;
     }
@@ -23,6 +39,8 @@ public class PlacePreview {
     public String getName() {
         return name;
     }
+
+    public String getTimeVisit() { return this.timeVisit;}
 
 
 }
